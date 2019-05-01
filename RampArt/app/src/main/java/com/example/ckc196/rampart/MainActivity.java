d7fbde65b2bbd001fe439837f78993ba9bafe99c
Chris Catzin <ckc196@humboldt.edu>
2019-04-30 23:41:09 -0700

    RampArt Update!

--- RampArt/app/src/main/java/com/example/ckc196/rampart/MainActivity.java
+++ RampArt/app/src/main/java/com/example/ckc196/rampart/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.ckc196.rampart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private EditText etUsername;
     private EditText etPassword;
@@ -36,6 +44,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Issue with Login");
+                    e.printStackTrace();
+                    return;
+                }
+
+                goMainActivity();
+
+            }
+        });
+
+    }
+
+    private void goMainActivity() {
+        Log.d(TAG, "Navigating to Ramp Art Activity");
+        Intent i = new Intent(this, RampArtActivity.class);
+        startActivity(i);
+        finish();
 
     }
 }
---
+++ RampArt/app/src/main/java/com/example/ckc196/rampart/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.ckc196.rampart;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("ramp-art-app") // should correspond to APP_ID env variable
+                .clientKey("RampArtSkatePark")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("http://ramp-art-app.herokuapp.com/parse").build());
+    }
+}
