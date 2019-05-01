3f1cf63d33f87135dfd7ef669345591d79440f95
Dajon Wise <ddw207@humboldt.edu>
2019-05-01 13:39:49 -0700

    added dajons work

--- app/src/main/java/com/example/rampart/MainActivity.java
+++ app/src/main/java/com/example/rampart/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         rellay_Chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Chat.class);
+                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
